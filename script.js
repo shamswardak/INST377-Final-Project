ec3efe25c2810428b89da15f821ef6d3628de494
shamswardak <wardakshams@gmail.com>
2023-04-23 21:21:56 -0400

    add event listener that collects inputted dates

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 async function mainEvent() {
     const form = document.querySelector("#date_form");
     const tableBody = document.querySelector("#earthquake_table tbody");
-    
+
+    form.addEventListener("submit", async (event) => {
+        startDate = document.querySelector("#start_date")
+        endDate = document.querySelector("#end_date")
+
+    })
 }
