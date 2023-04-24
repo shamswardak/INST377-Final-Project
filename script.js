b037064971c3a4317b1ec6f1beffea223d5d94d7
shamswardak <wardakshams@gmail.com>
2023-04-23 21:48:04 -0400

    fetch data from API and display to table

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ async function mainEvent() {
     const tableBody = document.querySelector("#earthquake_table tbody");
 
     form.addEventListener("submit", async (event) => {
-        startDate = document.querySelector("#start_date")
-        endDate = document.querySelector("#end_date")
+        event.preventDefault();
 
+        startDate = document.querySelector("#start_date").value;
+        endDate = document.querySelector("#end_date").value;
+
+        fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startDate}&endtime=${endDate}`) //An asynchronous data request to my API
+        .then((response) => response.json())
+        .then((data) => {
+          data.features.forEach((earthquake) => { // A processing request that uses array methods (forEach array method that iterates over each feature inside of an earthquake array)
+            const { mag, place, time } = earthquake.properties;
+            const row = document.createElement("tr");
+            row.innerHTML = `<td>${time}</td><td>${mag}</td><td>${place}</td>`;
+            tableBody.appendChild(row);
+          })
+        })
     })
-}
+    }
+    
+document.addEventListener("DOMContentLoaded", async () => {
+    await mainEvent();
+})
+      
